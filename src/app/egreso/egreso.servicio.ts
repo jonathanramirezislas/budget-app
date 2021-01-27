ed01300fb46bb1e36021f70f7073e613d56910a4
Jonathan Ramirez <68352096+jonathanramirezislas@users.noreply.github.com>
2021-01-27 12:44:24 -0600

    models-servicios esqueleto ingresos-egresos

---
+++ src/app/egreso/egreso.servicio.ts
@@ -0,0 +1,9 @@
+import { Egreso } from "./ingreso.model";
+
+export class EgresoServicio{
+    ingresos:Egreso[]=[
+        new Egreso("Renta dep",2000),
+        new Egreso("Ropa",500),
+
+    ];
+}
